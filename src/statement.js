4a4f6a0be8b7a101c61b73c8055ae044ea79ef6e
chivitc1 <chivitc1@gmail.com>
2019-06-20 22:44:35 +0700

    split phase: move volumeCreditsFor func

--- src/statement.js
+++ src/statement.js
@@ -9,6 +9,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -37,6 +38,14 @@ function statement(invoice, plays) {
         }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result =0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 10);
+        return result
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -63,17 +72,9 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result =0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 10);
-        return result
     }
 }
 
