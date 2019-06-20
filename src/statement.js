635a2e2fbe1603a73c6dc443424f7e85adbbcc4c
chivitc1 <chivitc1@gmail.com>
2019-06-20 21:45:27 +0700

    extract func renderPlainText() from statement()

--- src/statement.js
+++ src/statement.js
@@ -1,12 +1,13 @@
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n-------------------\n- You ordered:`;
-    
     for (let perf of invoice.performances) {
-        // print line for this order
         result += `\n---- ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience})`;
     }
-
     result += `\n- Amount owed is ${usd(totalAmount())}`;
     result += `\n- You earned ${totalVolumeCredits()} credits\n`;
     return result;
