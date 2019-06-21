63216599c81b7fb48a8585946b03b89ec5d648c1
chivitc1 <chivitc1@gmail.com>
2019-06-21 08:23:44 +0700

    move part of volumeCredits calc logic to subclasses - ComedyCalculator

--- src/createStatmentData.js
+++ src/createStatmentData.js
@@ -46,11 +46,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type)
-            result += Math.floor(this.performance.audience / 10);
-        return result
+        return Math.max(this.performance.audience - 30, 0)
     }
 }
 
@@ -72,6 +68,10 @@ class ComedyCalculator extends PerformanceCalculator {
         }
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 10);
     }
 }
 
